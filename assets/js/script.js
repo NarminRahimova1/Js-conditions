00c1d671b3873515bdc87fd085813829746ba1b3
NarminRahimova <nemorahimova11@gmail.com>
2024-01-25 10:17:12 +0400

    completed js-conditions

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let username = prompt("Enter your username");
+let password = prompt("Enter your password");
+let age = prompt("Enter your age");
+
+//Username = "Admin"
+//Password = "admin123"
+//Age>18
+
+if (username=="Admin" && password=="admin123" && age>"18") {
+   console.log("Siz sisteme daxil oldunuz...")
+}
